a09fb2335374785501282715b70551541955bc73
Mithun006 <48342777+Mithun006@users.noreply.github.com>
2019-07-04 00:18:24 +0530

    Update LCM.C

--- LCM.C
+++ LCM.C
@@ -2,39 +2,16 @@
 
 int main()
 {
-    int num1,num2,a,b,i=1,lcm=1;
+    int num1,num2,lcm;
     printf("Enter the 1st number and 2nd number: ") ;
     scanf("%d %d",&num1,&num2);
-    a=num1;
-    b=num2;
-    if(num1%num2==0 || num2%num1==0)
+    lcm=num1>num2?num1:num2;
+    while(1)
     {
-        if(num1>num2)
-            printf("LCM of %d and %d is %d",num1,num2,num1);
-        else
-            printf("LCM of %d and %d is %d",num1,num2,num2);
-        return 0;
+        if(lcm%num1==0 &&lcm%num2==0)
+            break;
+        lcm++;
     }
-    while(num1!=1 || num2!=1)
-    {
-        if(num1%i==0 && num2%i==0 && num1!=1 && num2!=1)
-        {
-            num1/=i;
-            num2/=i;
-            lcm*=i;
-        }
-        else if(num1%i==0 && num1!=1)
-        {
-            num1/=i;
-            lcm*=i;
-        }
-        else if(num2%i==0 && num2!=1)
-        {
-            num2/=i;
-            lcm*=i;
-        }
-        i++;
-    }
-    printf("LCM of %d and %d is %d",a,b,lcm);
+    printf("LCM of %d and %d is %d",num1,num2,lcm);
     return 0;
 }
